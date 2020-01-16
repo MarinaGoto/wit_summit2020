d19324c6de8db01b4f943ee98fd6c2f647a91343
marinagotovkina <marina@uppercase.no>
2020-01-16 16:47:05 +0100

    structure

---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+// Webpack uses this to work with directories
+const path = require('path');
+
+// webpack plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// This is main configuration object.
+// Here you write different options and tell Webpack what to do
+module.exports = {
+
+  // Path to your entry point. From this file Webpack will begin his work
+  entry: './src/main.js',
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on final bundle. For now we don't need production's JavaScript
+  // minifying and other thing so let's set mode to development
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        /* ... */
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 3,
+              sourceMap: false
+            }
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              sourceMap: false
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: false
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
+      },
+      {
+        // Apply rule for fonts files
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            // Using file-loader too
+            loader: "file-loader",
+            options: {
+              outputPath: 'fonts'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    //
+    // Webpack HTML Plugin
+    //
+    new HtmlWebpackPlugin({template: "./src/templates/index.html"}),
+    new MiniCssExtractPlugin({template: "./src/main.scss"}),
+  ],
+};
+
