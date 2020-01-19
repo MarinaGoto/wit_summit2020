5833b240030cdfcdef85bf0a001523f9984f393b
marinagotovkina <marina@uppercase.no>
2020-01-19 10:33:46 +0100

    - add content

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import './main.scss';
 import witLogo from './images/wit_summit_logo.svg';
 import unity from './images/unity.svg';
+import speaker from './images/speaker.png';
+import girls from './images/girls.png';
 
 const witLogoImg = document.getElementById('witLogo');
 witLogoImg.src = witLogo;
 
 const unityImg = document.getElementById('unity');
 unityImg.src = unity;
+
+const speakerImg = document.getElementById('speaker');
+speakerImg.src = speaker;
+
+const girlsImg = document.getElementById('girls');
+girlsImg.src = girls;
 
