212aee76140e5681da4c5e1e8a1b2a099c882334
marinagotovkina <marina@uppercase.no>
2020-01-27 17:44:24 +0100

    - add collapse functionality WIP

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import witLogo from './images/wit_summit_logo.svg';
 import unity from './images/unity.svg';
 import speaker from './images/speaker.png';
 import girls from './images/girls.png';
-import sopra from './images/sopra_logo.svg';
+import designer from './images/designer.jpg';
+import grandma from './images/grandma.png';
 
 const witLogoImg = document.getElementById('witLogo');
 witLogoImg.src = witLogo;
@@ -17,6 +18,9 @@ speakerImg.src = speaker;
 const girlsImg = document.getElementById('girls');
 girlsImg.src = girls;
 
-const sopraLogo = document.getElementById('sopra');
-sopraLogo.src = sopra;
+const designerImg = document.getElementById('designer');
+designerImg.src = designer;
+
+const grandmaImg = document.getElementById('grandma');
+grandmaImg.src = grandma;
 
