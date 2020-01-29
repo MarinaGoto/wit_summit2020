87a95022295b3a4e40f9d96559d6469d1086bf9a
marinagotovkina <marina@uppercase.no>
2020-01-29 22:04:11 +0100

    - updated content - add team, volunteer pic

--- src/main.js
+++ src/main.js
@@ -1,26 +1,10 @@
 import './main.scss';
 import witLogo from './images/wit_summit_logo.svg';
 import unity from './images/unity.svg';
-import speaker from './images/speaker.png';
-import girls from './images/girls.png';
-import designer from './images/designer.jpg';
-import grandma from './images/grandma.png';
 
 const witLogoImg = document.getElementById('witLogo');
 witLogoImg.src = witLogo;
 
 const unityImg = document.getElementById('unity');
 unityImg.src = unity;
-
-const speakerImg = document.getElementById('speaker');
-speakerImg.src = speaker;
-
-const girlsImg = document.getElementById('girls');
-girlsImg.src = girls;
-
-const designerImg = document.getElementById('designer');
-designerImg.src = designer;
-
-const grandmaImg = document.getElementById('grandma');
-grandmaImg.src = grandma;
 
