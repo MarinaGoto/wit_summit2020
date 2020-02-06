b4c25981611d0b512a6535536f28eda1f71dae55
Marina <marina.webdev@gmail.com>
2020-02-06 02:17:12 +0100

    - add small style fixes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,1 @@
 import './main.scss';
-import witLogo from './images/wit_summit_logo.svg';
-import unity from './images/unity.svg';
-
-const witLogoImg = document.getElementById('witLogo');
-witLogoImg.src = witLogo;
-
-const unityImg = document.getElementById('unity');
-unityImg.src = unity;
-
